997745d141a0a13cbd4fa9ee615252c947dcebe1
Pauline <gokowanjiru@gmail.com>
2023-03-31 15:41:35 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,25 @@
+# add
+def add(a,b):
+    answer = a+b
+    return answer
+
+
+# subtract
+def subtract(x,y):
+    subtraction = x-y
+    return subtraction
+
+ # divide
+def divide(r,s):
+    division = r/s
+    return division
+
+# multiply
+def multiply(c,d):
+    multiplication = c*d
+    return multiplication
+ 
+#  remainder
+def remainder(e,f):
+    modulus = e%f
+    return modulus
